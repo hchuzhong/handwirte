4f2f66060ff2d17b1b55b073c07b4cd6c72d381a
chuzhong <352995096@qq.com>
2021-10-25 23:25:52 +0800

    初步完成 promise A+ 标准

--- Promise/Promise.ts
+++ Promise/Promise.ts
@@ -1,19 +1,27 @@
 class Promise2 {
-    succeed = null;
-    fail = null;
+    state = "pending";
+    callbacks = [];
 
-    resolve() {
+    resolve(result) {
         setTimeout(() => {
-            if (typeof this.succeed === "function") {
-                this.succeed();
-            }
+            if (this.state !== "pending") return;
+            this.state = "fulfilled";
+            this.callbacks.forEach((handle) => {
+                if (typeof handle[0] === "function") {
+                    handle[0].call(undefined, result);
+                }
+            });
         });
     }
-    reject() {
+    reject(reason) {
         setTimeout(() => {
-            if (typeof this.fail === "function") {
-                this.fail();
-            }
+            if (this.state !== "pending") return;
+            this.state = "rejected";
+            this.callbacks.forEach((handle) => {
+                if (typeof handle[1] === "function") {
+                    handle[1].call(undefined, reason);
+                }
+            });
         });
     }
 
@@ -25,8 +33,14 @@ class Promise2 {
     }
 
     then(succeed?, fail?) {
-        this.succeed = succeed;
-        this.fail = fail;
+        const handle = [];
+        if (typeof succeed === "function") {
+            handle[0] = succeed;
+        }
+        if (typeof fail === "function") {
+            handle[1] = fail;
+        }
+        this.callbacks.push(handle);
     }
 }
 
--- Promise/test.ts
+++ Promise/test.ts
@@ -1,3 +1,4 @@
+import { doesNotReject } from "assert";
 import * as chai from "chai";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
@@ -44,5 +45,125 @@ describe("Promise", () => {
             });
         });
         promise.then(success);
+    });
+    it("promise.then(null, fail) 中的 fail 会在 reject 被调用的时候执行", (done) => {
+        const fail = sinon.fake();
+        const promise = new Promise((resolve, reject) => {
+            assert.isFalse(fail.called);
+            reject();
+            setTimeout(() => {
+                assert.isTrue(fail.called);
+                done();
+            });
+        });
+        promise.then(null, fail);
+    });
+    // promise A+ 规范
+    it("2.2.1", () => {
+        const promise = new Promise((resolve) => {
+            resolve();
+        });
+        promise.then(false, null);
+        assert(1 === 1);
+    });
+    it("2.2.2", (done) => {
+        const succeed = sinon.fake();
+        const promise = new Promise((resolve) => {
+            assert.isFalse(succeed.called);
+            resolve(1111);
+            resolve(2222);
+            setTimeout(() => {
+                assert(promise.state === "fulfilled");
+                assert.isTrue(succeed.calledOnce);
+                assert(succeed.calledWith(1111));
+                done();
+            });
+        });
+        promise.then(succeed);
+    });
+    it("2.2.3", (done) => {
+        const fail = sinon.fake();
+        const promise = new Promise((resolve, reject) => {
+            assert.isFalse(fail.called);
+            reject(1111);
+            reject(2222);
+            setTimeout(() => {
+                assert(promise.state === "rejected");
+                assert.isTrue(fail.calledOnce);
+                assert(fail.calledWith(1111));
+                done();
+            });
+        });
+        promise.then(null, fail);
+    });
+    it("2.2.4 我的代码执行完之前不能调用 then 中成功的函数", (done) => {
+        const succeed = sinon.fake();
+        const promise = new Promise((resolve) => {
+            resolve();
+        });
+        promise.then(succeed);
+        console.log(1);
+        assert.isFalse(succeed.called);
+        setTimeout(() => {
+            assert.isTrue(succeed.called);
+            done();
+        });
+    });
+    it("2.2.4 我的代码执行完之前不能调用 then 中失败的函数", (done) => {
+        const fail = sinon.fake();
+        const promise = new Promise((resolve, reject) => {
+            reject();
+        });
+        promise.then(null, fail);
+        console.log(1);
+        assert.isFalse(fail.called);
+        setTimeout(() => {
+            assert.isTrue(fail.called);
+            done();
+        });
+    });
+    it("2.2.5", (done) => {
+        const promise = new Promise((resolve, reject) => {
+            resolve();
+        });
+        promise.then(function () {
+            "use strict";
+            assert(this === undefined);
+            done();
+        });
+    });
+    it("2.2.6.1 then 可以在一个 promise 中被多次调用", (done) => {
+        const promise = new Promise((resolve, reject) => {
+            resolve();
+        });
+        const callbacks = [sinon.fake(), sinon.fake(), sinon.fake()];
+        promise.then(callbacks[0]);
+        promise.then(callbacks[1]);
+        promise.then(callbacks[2]);
+        setTimeout(() => {
+            assert(callbacks[0].called);
+            assert(callbacks[1].called);
+            assert(callbacks[2].called);
+            assert(callbacks[1].calledAfter(callbacks[0]));
+            assert(callbacks[2].calledAfter(callbacks[1]));
+            done();
+        });
+    });
+    it("2.2.6.2 then 可以在一个 promise 中被多次调用", (done) => {
+        const promise = new Promise((resolve, reject) => {
+            reject();
+        });
+        const callbacks = [sinon.fake(), sinon.fake(), sinon.fake()];
+        promise.then(null, callbacks[0]);
+        promise.then(null, callbacks[1]);
+        promise.then(null, callbacks[2]);
+        setTimeout(() => {
+            assert(callbacks[0].called);
+            assert(callbacks[1].called);
+            assert(callbacks[2].called);
+            assert(callbacks[1].calledAfter(callbacks[0]));
+            assert(callbacks[2].calledAfter(callbacks[1]));
+            done();
+        });
     });
 });
